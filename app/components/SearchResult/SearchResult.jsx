b34192a68f13de3fbe92c61b81a5ab98ef0e3222
Icarus1989 <53438706+Icarus1989@users.noreply.github.com>
2024-06-11 16:51:06 +0200

    found bug in MainComponent search part - deep changes

--- app/components/SearchResult/SearchResult.jsx
+++ app/components/SearchResult/SearchResult.jsx
@@ -8,30 +8,6 @@ import styles from "./SearchResult.module.css";
 export default function SearchResult({ id, title, image, saved }) {
 	return (
 		<li className={styles["recipe"]} key={id}>
-			<svg viewBox="0 0 20 10" className={styles["recipe-image-container"]}>
-				<circle
-					cx="18%"
-					cy="50%"
-					r="60%"
-					fill="url(#myCircleGradient)"
-					stroke="url(#myCircleGradient)"
-				/>
-				<defs>
-					<linearGradient id="myCircleGradient">
-						<stop offset="0%" stopColor="green" />
-						<stop offset="70%" stopColor="#232323" />
-					</linearGradient>
-				</defs>
-			</svg>
-			<Image
-				className={styles["recipe-image"]}
-				src={image}
-				alt={`${title} image`}
-				style={{ overflow: "hidden" }}
-				width={312}
-				height={231}
-			/>
-
 			<Link
 				className={styles["recipe-link"]}
 				href={`/search/${id}${saved ? "/saved" : ""}`}
@@ -39,6 +15,29 @@ export default function SearchResult({ id, title, image, saved }) {
 				<span className={styles["recipe-title"]}>
 					{title.length > 50 ? `${title.slice(0, 51)}...` : title}
 				</span>
+				<svg viewBox="0 0 20 10" className={styles["recipe-image-container"]}>
+					<circle
+						cx="18%"
+						cy="50%"
+						r="60%"
+						fill="url(#myCircleGradient)"
+						stroke="url(#myCircleGradient)"
+					/>
+					<defs>
+						<linearGradient id="myCircleGradient">
+							<stop offset="0%" stopColor="#0f8b0f" />
+							<stop offset="95%" stopColor="#232323" />
+						</linearGradient>
+					</defs>
+				</svg>
+				<Image
+					className={styles["recipe-image"]}
+					src={image}
+					alt={`${title} image`}
+					style={{ overflow: "hidden" }}
+					width={312}
+					height={231}
+				/>
 			</Link>
 		</li>
 	);
